6b5aa0bce4f42c5ad1d09ac8629bc00016904769
mand32128 <kojimandokoro1228@gmail.com>
2022-08-27 17:39:02 +0900

    add Card class

---
+++ card.ts
@@ -0,0 +1,39 @@
+const CardType = {
+    NORMAL: 'normal',
+    JOKER: 'joker',
+} as const;
+
+const Suit = {
+    SPADE: 'spade',
+    HEART: 'heart',
+    DIAMOND: 'diamong',
+    CLUB: 'club',
+} as const;
+
+const Rank = {
+    ACE: 1,
+    TWO: 2,
+    THREE: 3,
+    FOUR: 4,
+    FIVE: 5,
+    SIX: 6,
+    SEVEN: 7,
+    EIGHT: 8,
+    NINE: 9,
+    TEN: 10,
+    JACK: 11,
+    QUEEN: 12,
+    KING: 13,
+} as const;
+
+type CardType = typeof CardType[keyof typeof CardType];
+type Suit = typeof Suit[keyof typeof Suit];
+type Rank = typeof Rank[keyof typeof Rank];
+
+class Card {
+    constructor(
+        private type: CardType,
+        private suit: Suit,
+        private rank: Rank
+        ) {}
+}
