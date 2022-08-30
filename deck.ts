04d7386b55544456fa8f25e38e2fd7f4d5b5a5ff
mand32128 <kojimandokoro1228@gmail.com>
2022-08-30 19:47:33 +0900

    mod card class to type

--- deck.ts
+++ deck.ts
@@ -1,4 +1,40 @@
-import * as card from "./card"
+const CardType = {
+    NORMAL: 'normal',
+    JOKER: 'joker',
+} as const;
+
+const Suit = {
+    SPADE: 'spade',
+    HEART: 'heart',
+    DIAMOND: 'diamong',
+    CLUB: 'club',
+} as const;
+
+const Rank = {
+    ACE: 1,
+    TWO: 2,
+    THREE: 3,
+    FOUR: 4,
+    FIVE: 5,
+    SIX: 6,
+    SEVEN: 7,
+    EIGHT: 8,
+    NINE: 9,
+    TEN: 10,
+    JACK: 11,
+    QUEEN: 12,
+    KING: 13,
+} as const;
+
+type CardType = typeof CardType[keyof typeof CardType];
+type Suit = typeof Suit[keyof typeof Suit];
+type Rank = typeof Rank[keyof typeof Rank];
+
+type Card = {
+    cardType: CardType,
+    suit: Suit,
+    rank: Rank,
+};
 
 class Deck {
     private remainingCards: card.Card[];
